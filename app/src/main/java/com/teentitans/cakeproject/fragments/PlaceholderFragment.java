5ff2fe0a15b1d7e458da0a3c6bf9895a5bffc205
cretzuwgz <bboy_alex93@yahoo.com>
2015-03-30 14:31:33 +0300

    Initial commit

---
+++ app/src/main/java/com/teentitans/cakeproject/fragments/PlaceholderFragment.java
@@ -0,0 +1,8 @@
+package com.teentitans.cakeproject.fragments;
+
+/**
+ * Placeholder class for project hierarchy.
+ * Delete this when you create other class in this package.
+ */
+public class PlaceholderFragment {
+}
---
+++ app/src/main/java/com/teentitans/cakeproject/utils/PlaceholderUtils.java
@@ -0,0 +1,8 @@
+package com.teentitans.cakeproject.utils;
+
+/**
+ * Placeholder class for project hierarchy.
+ * Delete this when you create other class in this package.
+ */
+public class PlaceholderUtils {
+}
