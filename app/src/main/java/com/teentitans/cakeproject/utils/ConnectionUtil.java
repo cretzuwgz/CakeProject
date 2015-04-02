5296e0315c613f36fff73c447c24119bc51ff5f5
Gicu93 <s.florik93@yahoo.com>
2015-04-02 18:02:26 +0300

    ConnectionUtil

---
+++ app/src/main/java/com/teentitans/cakeproject/utils/ConnectionUtil.java
@@ -0,0 +1,70 @@
+package com.teentitans.cakeproject.utils;
+
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ConnectionUtil {
+
+    public static String getResponseFromURL(String link) throws IOException{
+
+        HttpURLConnection connection;
+
+        URL url = new URL(link);
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestMethod("POST");
+
+        String line;
+        InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+        BufferedReader reader = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        String response = sb.toString();
+        isr.close();
+        reader.close();
+        connection.disconnect();
+
+        return response;
+    }
+
+    public static String getResponseFromURL(String link, String parameters) throws IOException{
+
+        HttpURLConnection connection;
+        OutputStreamWriter request;
+
+        URL url = new URL(link);
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestMethod("POST");
+
+        request = new OutputStreamWriter(connection.getOutputStream());
+        request.write(parameters);
+        request.flush();
+        request.close();
+
+        String line;
+        InputStreamReader isr = new InputStreamReader(connection.getInputStream());
+        BufferedReader reader = new BufferedReader(isr);
+        StringBuilder sb = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            sb.append(line).append("\n");
+        }
+        String response = sb.toString();
+        isr.close();
+        reader.close();
+        connection.disconnect();
+
+        return response;
+    }
+}
